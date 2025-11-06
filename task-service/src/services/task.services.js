6716644e7426ac8ee67557534161421845cf2b02
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-11-06 22:03:57 +0530

    services for tasks

---
+++ task-service/src/services/task.services.js
@@ -0,0 +1,25 @@
+import Task from "../models/task.model";
+import { publishTaskEvent } from "../queues/producer";
+export async function createTask(title, description, assignedTo) {
+  if (!title || !description || !assignedTo) throw new Error("Invalid Request");
+  const task = await Task.create({ title, description, assignedTo });
+  await publishTaskEvent("created", {
+    id: task._id,
+    title: task.title,
+    assignedTo: task.assignedTo,
+  });
+  return task;
+}
+export async function assignTask(taskId, username) {
+  if (!taskId || !username) throw new Error("Invalid request");
+  const task = Task.findById(taskId);
+  if (!task) throw new Error("No Task found");
+  task.assignedTo = username;
+  task.status = "in-progress";
+  await task.save();
+  await publishTaskEvent("assigned", { taskId: task._id, username });
+  return task;
+}
+export async function getAllTasks() {
+  return await Task.find();
+}
