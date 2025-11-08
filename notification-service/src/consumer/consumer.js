dca37552573915fa3ca3d92d8a95f1f283f2838b
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-11-08 12:17:37 +0530

    consumer queue created

---
+++ notification-service/src/consumer/consumer.js
@@ -0,0 +1,51 @@
+//bind the user and task quesues to create notifications
+import { getChannel } from "../config/rabbitmq.js";
+import { createNotification } from "../services/notification.service.js";
+export async function consumeMessages() {
+  const channel = getChannel();
+  const queue = "notification.queue";
+  await channel.assertQueue(queue, { durable: true }); //create queue
+  //blind to user and task queue
+  await channel.bindQueue(queue, "events.exchange", "user.*");
+  await channel.bindQueue(queue, "events.exchange", "task.*");
+  console.log("Wasiting for messages");
+  channel.consume(queue, async (msg) => {
+    const data = JSON.parse(msg.content.toString()); //convert to JSON
+    console.log("Received:", data);
+    try {
+      switch (
+        data.event //create the notification accordingly to the event
+      ) {
+        case "created":
+          if (data.payload.email) {
+            await createNotification(
+              "user.created",
+              `New user registered: ${data.payload.email}`,
+              data.payload.id
+            );
+          } else if (data.payload.title) {
+            await createNotification(
+              "task.created",
+              `New Task created: ${data.payload.title}`,
+              null,
+              data.payload.id
+            );
+          }
+          break;
+        case "assigned":
+          await createNotification(
+            "task.assigned",
+            `Task ${data.payload.taskId} assigned to user ${data.payload.userId}`,
+            data.payload.userId,
+            data.payload.taskId
+          );
+          break;
+        default:
+          console.log("Event not recognized");
+      }
+      channel.ack(msg); //send the acknowledgement to the channel
+    } catch (error) {
+      console.error("Error processing message:", err.message);
+    }
+  });
+}
